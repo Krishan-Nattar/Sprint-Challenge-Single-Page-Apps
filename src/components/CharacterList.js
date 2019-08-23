05370ac396636b842a7cec6e76a52756e8a60712
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-23 09:33:56 -0700

    started adding searchform functionality

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import CharacterCard from './CharacterCard';
+import SearchForm from './SearchForm';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -18,10 +19,13 @@ if(!characters){
   return null
 }
   return (
+    <div>
+      <SearchForm />
     <section className="character-list grid-view">
       {characters.map(character=>{
         return <CharacterCard key={character.id} character={character} />
       })}
     </section>
+    </div>
   );
 }
