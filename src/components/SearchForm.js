00bf3f4c095e63af78ef13efe96362ea61981c93
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-23 09:36:11 -0700

    input form now controlled via state

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,12 +2,20 @@ import React, { useState } from "react";
 
 export default function SearchForm({ onSearch }) {
   // STRETCH TODO: Add stateful logic for query/form data
+  const [name, setName] = useState('');
+
+  const handleInputChange = (e) =>{
+    // console.log(e.target.value);
+    setName(e.target.value);
+  }
+
+
   return (
     <section className="search-form">
       <form onSubmit={() => onSearch(name)}>
         <input
           onChange={handleInputChange}
-          placeholder="name"
+          placeholder="Search"
           value={name}
           name="name"
         />
