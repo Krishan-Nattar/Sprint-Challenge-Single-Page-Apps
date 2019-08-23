5bd8abfd9ff5cb09dea134d3ecbaba9e2b51f9b9
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-23 09:49:38 -0700

    episode search working

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import EpisodeCard from "./EpisodeCard";
+import SearchForm from './SearchForm';
 
 const EpisodeList = () => {
   const [episodes, setEpisodes] = useState();
+  const [page, setPage] = useState('');
 
   useEffect(() => {
-    axios.get("https://rickandmortyapi.com/api/episode/").then(response => {
+    axios.get(`https://rickandmortyapi.com/api/episode/?name=${page}`).then(response => {
       setEpisodes(response.data.results);
     });
-  }, []);
+  }, [page]);
 
   if (!episodes) {
     return null;
   }
 
+  const onSearch = (search) =>{
+    setPage(search);
+  }
+
   return (
+    <div>
+      <SearchForm  onSearch={onSearch}/>
+    
     <section className="episode-list grid-view">
       {episodes.map(episode => {
         return <EpisodeCard key={episode.id} episode={episode} />;
       })}
     </section>
+    </div>
   );
 };
 
